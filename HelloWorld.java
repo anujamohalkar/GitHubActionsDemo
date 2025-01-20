93c215b6591a84217440a4ae03e976dd6efde89b
anujamohalkar <104577111+anujamohalkar@users.noreply.github.com>
2025-01-20 18:16:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from ANUJA");
+        System.out.println("GitHub Action Demo");
     }
 }
